5f7582c67dd8a6d472044e3cfb4123eee0a2d74d
belasairy <123364034+MEHDIBELAS@users.noreply.github.com>
2023-03-28 01:59:46 +0100

    comm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,27 @@
 #include "main.h"
+
 /**
- * puts2 - prints one char out of 2 of a string.
- * @str: input string.
- * Return: no return.
+ * puts2 - prints every other character of a string
+ * @str: char to check
  */
-void puts2(char *str)
+
+void	puts2(char *str)
 {
-int count = 0;
-while (count >= 0)
-{       
-if (str[count] == '\0')
-{       
-_putchar('\n');
-break;  
-}       
-if (count % 2 == 0) 
-_putchar(str[count]);
-count++;
-}       
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
