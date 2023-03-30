164539da24dcda1d1fda66e5e81422a165df9777
belasairy <123364034+MEHDIBELAS@users.noreply.github.com>
2023-03-30 13:31:05 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,10 +6,6 @@
  * @n: amount of bytes used from src.
  * Return: the pointer to dest.
  */
-char *_strncat(char *dest, char *src, int n)
-{
-int count = 0, count2 = 0;
-while (*(dest + count) != '\0')
 {
 count++;
 }
